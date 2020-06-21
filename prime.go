75574560ab73876d503f1a64f3a835c5816faff6
ajcuk <daniel.waechtler@student.uni-halle.de>
2020-06-21 14:28:20 +0200

    prime

--- prime.go
+++ prime.go
@@ -1,5 +1,12 @@
 package gomath
 
-func prime(a int, b int) int {
-	return a + b
+import "math"
+
+func IsPrime(number int) bool {
+	for i := 2; i <= int(math.Floor(math.Sqrt(float64(number)))); i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
 }
